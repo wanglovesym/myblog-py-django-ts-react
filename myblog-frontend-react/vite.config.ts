30222147aeff886b225101a0e0057e97cffe95ca
Jayden Wang <jayden@JaydendeMacBook-Pro.local>
2025-11-25 08:41:11 +0800

    feat: connect to backend server

--- myblog-frontend-react/vite.config.ts
+++ myblog-frontend-react/vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": {
-                target: "http://localhost:8000",
+                // target: "http://localhost:8000",
+                target: "https://myblog-backend-django.onrender.com",
                 changeOrigin: true
             }
         }
