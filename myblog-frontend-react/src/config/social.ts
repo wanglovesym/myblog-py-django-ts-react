ee1b28a2c940bf4b17177fda752c9adcc40ea35c
Jayden Wang <jayden@JaydendeMacBook-Pro.local>
2025-12-01 23:40:03 +0800

    feat: add file to store social media links

---
+++ myblog-frontend-react/src/config/social.ts
@@ -0,0 +1,7 @@
+export const SOCIAL = {
+    github:
+        import.meta.env.VITE_SOCIAL_GITHUB || "https://github.com/wanglovesym",
+    discord: import.meta.env.VITE_SOCIAL_DISCORD || "#",
+    wechat: import.meta.env.VITE_SOCIAL_WECHAT || "wanglovesym",
+    email: import.meta.env.VITE_SOCIAL_EMAIL || "mailto:shixinw998@gmail.com"
+};
